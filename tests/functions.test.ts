cc56206b952c7e6ffe2f1c492ac84324c795b038
ofk <ofkjpn+github@gmail.com>
2023-11-19 19:40:03 +0900

    Refactoring test

---
+++ tests/functions.test.ts
@@ -0,0 +1,43 @@
+import { blend } from 'culori';
+
+import { extendMatchers } from './utils';
+import { color, gray, toColor, translucent } from '../src';
+
+beforeEach(extendMatchers);
+
+describe('functions', () => {
+  test('toColor', () => {
+    expect(toColor('red')).toBeCloseToColor('#ff0000');
+    expect(toColor('invalid')).toBeCloseToColor('#000000');
+  });
+
+  test('translucent', () => {
+    expect(translucent('white', 'black', '#999')).toBeCloseToColor('#00000066');
+    expect(translucent('black', 'white', '#999')).toBeCloseToColor('#ffffff99');
+    expect(translucent('white', '#999')).toBeCloseToColor('#00000066');
+    expect(translucent('black', '#999')).toBeCloseToColor('#ffffff99');
+  });
+
+  test('gray', () => {
+    ['white', 'black'].forEach((background) => {
+      [1, 1.5, 15].forEach((contrast) => {
+        expect(gray(background, contrast)).toBeCloseToContrast(contrast, background);
+      });
+      expect(gray(gray(background, 1.5), -1.5)).toBeCloseToColor(background);
+      expect(gray(gray(background, 15), 15)).toBeCloseToColor(background);
+      expect(gray(background, 1.5, 0.5).alpha).toBeCloseTo(0.5);
+      expect(blend([background, gray(background, 1.5, 0.5)])).toBeCloseToContrast(1.5, background);
+    });
+  });
+
+  test('color', () => {
+    ['white', 'black'].forEach((background) => {
+      [0, 30, 210].forEach((hue) => {
+        [1.5, 4].forEach((contrast) => {
+          expect(color(background, contrast, hue)).toBeCloseToContrast(contrast, background);
+          expect(color(background, contrast, hue)).toBeCloseToHue(hue);
+        });
+      });
+    });
+  });
+});
---
+++ tests/utils.ts
@@ -0,0 +1,46 @@
+import { differenceCiede2000, differenceHueNaive, formatCss, hsl, wcagContrast } from 'culori';
+import type { Color } from 'culori';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R> {
+      /* eslint-disable @typescript-eslint/method-signature-style */
+      toBeCloseToColor(expected: Color | string): R;
+      toBeCloseToContrast(contrast: number, background: Color | string): R;
+      toBeCloseToHue(hue: number, tolerance?: number): R;
+      /* eslint-enable @typescript-eslint/method-signature-style */
+    }
+  }
+}
+
+export const extendMatchers = (): void => {
+  expect.extend({
+    toBeCloseToColor(received: Color | string, expected: Color | string): jest.CustomMatcherResult {
+      const pass = differenceCiede2000()(received, expected) < 0.2;
+      const message = (): string =>
+        `expected ${formatCss(received)} ${pass ? '' : 'not '}to be ${formatCss(expected)}`;
+      return { pass, message };
+    },
+    toBeCloseToContrast(
+      received: Color | string,
+      contrast: number,
+      background: Color | string,
+    ): jest.CustomMatcherResult {
+      const actualContrast = wcagContrast(received, background);
+      const pass = actualContrast >= contrast && actualContrast < contrast * 1.2;
+      const message = (): string =>
+        `expected contrast ${actualContrast} between ${formatCss(received)} and ${formatCss(
+          background,
+        )} ${pass ? '' : 'not '}to be equal contrast ${contrast}`;
+      return { pass, message };
+    },
+    toBeCloseToHue(received: Color | string, hue: number, tolerance = 2): jest.CustomMatcherResult {
+      const actualHue = hsl(received)?.h ?? 0;
+      const pass = Math.abs(differenceHueNaive({ h: actualHue }, { h: hue })) < tolerance;
+      const message = (): string =>
+        `expected hue ${actualHue} of ${formatCss(received)} ${pass ? '' : 'not '}to be hue ${hue}`;
+      return { pass, message };
+    },
+  });
+};
