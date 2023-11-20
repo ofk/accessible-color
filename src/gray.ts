bd0b4fec44002998b07b643955e51428c1002351
ofk <ofkjpn+github@gmail.com>
2023-11-20 22:35:21 +0900

    Add adjustLuminance

--- src/gray.ts
+++ src/gray.ts
@@ -1,5 +1,6 @@
-import type { Color } from 'culori';
-import { clampRgb, wcagLuminance } from 'culori';
+import type { Oklch } from 'culori';
+
+import { adjustLuminance } from './luminance';
 
 // cf. https://github.com/Evercoder/culori/blob/main/src/wcag.js
 //     https://github.com/Evercoder/culori/blob/main/src/lrgb/convertRgbToLrgb.js
@@ -11,21 +12,7 @@ const calcGrayColorValue = (luminance: number): number => {
   return (Math.sign(luminance) || 1) * (luminance ** (1 / 2.4) * 1.055 - 0.055);
 };
 
-export const calcGrayColor = (luminance: number, direction = 0): Color => {
+export const calcGrayColor = (luminance: number, direction = 0): Oklch => {
   const v = calcGrayColorValue(luminance) || 0;
-  const color = clampRgb({ mode: 'rgb' as const, r: v, g: v, b: v });
-
-  if (direction > 0) {
-    if (wcagLuminance(color) < luminance) {
-      const u = v + 1 / 255;
-      return clampRgb({ mode: 'rgb' as const, r: u, g: u, b: u });
-    }
-  } else if (direction < 0) {
-    if (wcagLuminance(color) > luminance) {
-      const u = v - 1 / 255;
-      return clampRgb({ mode: 'rgb' as const, r: u, g: u, b: u });
-    }
-  }
-
-  return color;
+  return adjustLuminance({ mode: 'rgb', r: v, g: v, b: v }, luminance, direction);
 };
--- src/luminance.ts
+++ src/luminance.ts
@@ -1,5 +1,8 @@
 // cf. https://github.com/Evercoder/culori/blob/main/src/wcag.js
 
+import type { Color, Oklch } from 'culori';
+import { clampChroma, oklch, wcagLuminance } from 'culori';
+
 const calcBrightLuminance = (backgroundLuminance: number, contrast: number): number =>
   (backgroundLuminance + 0.05) * contrast - 0.05;
 
@@ -13,4 +16,25 @@ export const calcLuminance = (backgroundLuminance: number, signedContrast: numbe
   }
   if (signedContrast > 0) return calcBrightLuminance(backgroundLuminance, signedContrast);
   return calcDarkLuminance(backgroundLuminance, -signedContrast);
+};
+
+export const adjustLuminance = (
+  baseColor: Color,
+  luminance: number,
+  direction = 0,
+  range = 1,
+  steps = 1000,
+): Oklch => {
+  const baseOklch = oklch(baseColor);
+  let color = clampChroma(baseOklch, 'oklch');
+  if (direction > 0) {
+    for (let i = 0; i < range * steps && wcagLuminance(color) < luminance; i += 1) {
+      color = clampChroma({ ...baseOklch, l: baseOklch.l + i / steps }, 'oklch');
+    }
+  } else if (direction < 0) {
+    for (let i = 0; i < range * steps && wcagLuminance(color) > luminance; i += 1) {
+      color = clampChroma({ ...baseOklch, l: baseOklch.l - i / steps }, 'oklch');
+    }
+  }
+  return color;
 };
