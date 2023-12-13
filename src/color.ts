feebbda3d3fbbaa8055d144837fc2be2e2c74f1c
ofk <ofkjpn+github@gmail.com>
2023-12-14 00:43:41 +0900

    Add adjustChromaByNeighbor

--- src/color.ts
+++ src/color.ts
@@ -1,15 +1,32 @@
 import type { Color, Oklch } from 'culori';
-import { clampChroma, interpolate, lab, oklab, oklch, samples, wcagLuminance } from 'culori';
+import {
+  average,
+  clampChroma,
+  interpolate,
+  lab,
+  oklab,
+  oklch,
+  samples,
+  wcagLuminance,
+} from 'culori';
+import type { NonEmptyArray } from 'culori/src/common';
 
 import { calcGrayColor } from './gray';
 import { adjustLuminance } from './luminance';
+
+interface SafeOklch extends Omit<Oklch, 'h'>, Required<Pick<Oklch, 'h'>> {}
+
+const toSafeOklch = (color: Color): SafeOklch => {
+  const c = clampChroma(oklch(color), 'oklch');
+  return { ...c, c: c.c || 0, h: c.h ?? 0 };
+};
 
 export const calcBoldColor = (backgroundColor: Color, targetColor: Color): Oklch => ({
   ...oklch(targetColor),
   l: wcagLuminance(backgroundColor) < 0.5 ? 1 : 0,
 });
 
-const calcMostLiveColor = (hue: number, count = 20): Oklch => {
+const calcMostLiveColor = (hue: number, count = 20): SafeOklch => {
   const baseColors = interpolate(
     [
       { mode: 'oklch', l: 0, c: 1, h: hue },
@@ -18,13 +35,29 @@ const calcMostLiveColor = (hue: number, count = 20): Oklch => {
     'oklch',
   );
   return samples(count)
-    .map((v) => clampChroma(baseColors(v), 'oklch'))
-    .reduce((prevColor, color) => ((prevColor.c || 0) < (color.c || 0) ? color : prevColor));
+    .map((v) => toSafeOklch(baseColors(v)))
+    .reduce((prevColor, color) => (prevColor.c < color.c ? color : prevColor));
+};
+
+const adjustChromaByNeighbor = (baseColor: Oklch, hueTolerance: number, count = 20): SafeOklch => {
+  const color = toSafeOklch(baseColor);
+  const neighborColors = interpolate(
+    [
+      { ...color, h: color.h - hueTolerance },
+      { ...color, h: color.h + hueTolerance },
+    ],
+    'oklch',
+  );
+  const averageColor = average(
+    samples(count).map((v) => toSafeOklch(neighborColors(v))) as NonEmptyArray<SafeOklch>,
+    'oklch',
+  );
+  return { ...color, c: averageColor.c };
 };
 
 export const calcColor = (luminance: number, hue: number, direction = 0): Oklch => {
   const grayColor = calcGrayColor(luminance);
-  const liveColor = calcMostLiveColor(hue);
+  const liveColor = adjustChromaByNeighbor(calcMostLiveColor(hue), 60);
   const mutedColor = clampChroma({ ...lab(liveColor), l: lab(grayColor).l }, 'lab');
   const resColor = clampChroma({ ...oklab(liveColor), l: oklab(mutedColor).l }, 'oklab');
   const negColor = adjustLuminance(resColor, luminance, -direction);
